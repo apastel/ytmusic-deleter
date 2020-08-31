eb2f487e55b441bfac36e3b429a1ad60a97dfc07
apastel <alex.r.pastel@gmail.com>
2020-08-30 23:12:51 -0700

    Delete albums in loop

---
+++ main.py
@@ -0,0 +1,20 @@
+from ytmusicapi import YTMusic
+youtube_auth = YTMusic('headers_auth.json')
+
+while True:
+    uploaded_albums = youtube_auth.get_library_upload_albums()
+    if not len(uploaded_albums) > 0:
+        break
+    for album in uploaded_albums:
+        try:
+            artist = album.get('artists')[0].get('name') if len(album.get('artists')) > 0 else "Unknown Artist"
+            title = album.get("title")
+            response = youtube_auth.delete_upload_entity(album['browseId'])
+            if response == 'STATUS_SUCCEEDED':
+                print(f'Deleted {artist} - {title}')
+            else:
+                print(f'Failed to delete {artist} - {title}')
+        except (AttributeError, TypeError):
+            pass
+
+print('Finished deleting albums!')
