e50e483d8e93f80c864503206da63dad5000d6af
apastel <alex.r.pastel@gmail.com>
2022-03-29 22:44:39 -0600

    upgrade version of ytmusicapi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from _version_git import __version__, get_cmdclass  # noqa
 
 module_name = "ytmusic-deleter"
 
-install_reqs = ["click", "ytmusicapi >= 0.19.3", "enlighten"]
+install_reqs = ["click", "ytmusicapi >= 0.21.0", "enlighten"]
 
 develop_reqs = ["pre-commit", "flake8", "yapf", "coverage", "rope", "pytest"]
 
