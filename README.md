38bdaf16db3989bade154984cef36909a8dffb44
Alex Pastel <alex.r.pastel@gmail.com>
2022-05-21 14:01:23 -0600

    use QProcess to execute ytmusic-deleter commands and write output to textedit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ytmusic-deleter-packager
 A project that aims to package ytmusic-deleter into an easy to install &amp; run application
+
+# ToDo
+~~* Print stdout to in-app textarea instead of separate terminal window~~
+* Don't require running as administrator
+* Initial screen for inputting request headers (instead of separate terminal window)
+* Copy ytmusicapi/locales from site-packages instead of including in the project tree
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,26 +1,60 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QProcess
 from ui_form import Ui_MainWindow
-from ytmusic_deleter import cli
-from PyQt5.QtCore import pyqtSlot
-
 import sys
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.p = None
         self.removeLibraryButton.clicked.connect(self.remove_library)
         self.deleteUploadsButton.clicked.connect(self.delete_uploads)
 
-    @pyqtSlot()
-    def remove_library(self):
-        cli.remove_library(standalone_mode=False)
-        
-    @pyqtSlot()
-    def delete_uploads(self):
-        cli.delete_uploads(standalone_mode=False)
+    def launch_process(self, args):
+        if self.p is None:
+            self.message("Executing process.")
+            self.p = QProcess()
+            self.p.readyReadStandardOutput.connect(self.handle_stdout)
+            self.p.readyReadStandardError.connect(self.handle_stderr)
+            self.p.stateChanged.connect(self.handle_state)
+            self.p.finished.connect(self.process_finished)
+            self.p.start("ytmusic-deleter", args)
 
+    def remove_library(self):
+        self.launch_process(["remove-library"])
+    
+    def delete_uploads(self):
+        self.launch_process(["delete-uploads"])
+
+    def handle_stderr(self):
+        data = self.p.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        self.message(stderr)
+
+    def handle_stdout(self):
+        data = self.p.readAllStandardOutput()
+        stdout = bytes(data).decode("utf8")
+        self.message(stdout)
+
+    def handle_state(self, state):
+        states = {
+            QProcess.NotRunning: 'Not running',
+            QProcess.Starting: 'Starting',
+            QProcess.Running: 'Running',
+        }
+        state_name = states[state]
+        self.message(f"State changed: {state_name}")
+
+    def process_finished(self):
+        self.message("Process finished.")
+        self.p = None
+
+    def message(self, msg):
+        self.consoleTextArea.appendPlainText(msg)
+    
 if __name__ == '__main__':
     appctxt = ApplicationContext()
     window = MainWindow()
