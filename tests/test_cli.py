dac794797fba5c54682bd06ae1c333b1353c43f4
apastel <alex.r.pastel@gmail.com>
2024-09-23 20:29:00 -0600

    assert that history is empty after test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -90,6 +90,8 @@ class TestCli:
 
         items_deleted = result.return_value
         assert items_deleted >= len(sample_long_song_list), "One or more history items were not deleted"
+        with pytest.raises(Exception, match="None"):
+            yt_oauth.get_history()
 
     def test_delete_playlists(self, yt_oauth: YTMusic, create_playlist_and_delete_after):
         runner = CliRunner()
@@ -131,29 +133,3 @@ class TestCli:
         assert 2 == len(
             check_for_duplicates(processed_playlist, yt_oauth)
         ), "Playlist contained wrong number of remaining duplicates"
-
-    def test_delete_all(
-        self,
-        yt_browser: YTMusic,
-        upload_song,
-        add_library_album,
-        add_podcast,
-        create_playlist_and_delete_after,
-        like_song,
-        add_history_items,
-    ):
-        runner = CliRunner()
-        result = runner.invoke(cli, ["delete-all"], standalone_mode=False, obj=yt_browser)
-        print(result.stdout)
-        assert result.exit_code == 0
-
-        # Verify everything was deleted
-        assert 0 == len(yt_browser.get_library_upload_songs())
-        assert 0 == len(yt_browser.get_library_songs())
-        assert 1 == len(yt_browser.get_library_podcasts())
-        assert "Episodes you save for later" == yt_browser.get_library_podcasts()[0].get("channel").get("name")
-        assert 1 == len(yt_browser.get_library_playlists())
-        assert "Episodes you save for later" == yt_browser.get_library_playlists()[0].get("description")
-        assert 0 == len(yt_browser.get_liked_songs()["tracks"])
-        with pytest.raises(Exception, match="None"):
-            yt_browser.get_history()
